6e6bf26ac948493f75453c80dae2a768e0865e3d
Admin <Admin@LAPTOP-2LGM8HPC>
2021-08-24 14:44:48 +0700

    1

--- Baitap2/Models/GiaiPhuongTrinh.vb
+++ Baitap2/Models/GiaiPhuongTrinh.vb
@@ -1,6 +1,20 @@
-﻿Public Class GiaiPhuongTrinh
+﻿Using System;
+Using System.Collections.Generic;
+Using .Linq
+Using .System.Web.Mvc;
+Using DemoMVS.Models;
+
+
+Public Class GiaiPhuongTrinh
     {
-        Public
+        Public Double GiaiPhuongTrinhBacNhat(Double a, Double b)
+        {
+            Double x;
+        x = -b/a
+            //Return x;
+        }
+    }
+
 
 
 End Class
